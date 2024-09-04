265f8b17596ac947466b5837f35904998e2a561e
crownlessprince <shonkalangutkar1998@gmail.com>
2024-09-04 21:33:45 +0530

    this is my frist file of dsa

---
+++ frist.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+//    cout<< "namaste duniya" <<endl;
+   int a = 123;
+   cout<< a <<endl;
+
+   char ch = 'a';
+   cout<< ch <<endl;
+
+   bool bl = true;
+   cout<< bl <<endl;
+
+   float fl = 12.3;
+   cout<< fl <<endl;
+
+   double dl = 1.2;
+   cout<< dl <<endl;
+
+  int size = sizeof(a);
+  cout << "size of a: " << size << endl;
+}
